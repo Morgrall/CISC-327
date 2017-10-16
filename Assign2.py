565b4b44a010859e2cca6c2e722a22fb7638c2a2
Tyler Searl <14tjs6@caslab.queensu.ca>
2017-10-16 16:02:01 -0400

    adding test accounts file for coding inputs

--- Assign2.py
+++ Assign2.py
@@ -1,11 +1,33 @@
-def main():
-    print "hello world!"
+isPrivileged = True
+
+def Main():
+    while True:
+        command = input()
+        if command.upper() == "LOGIN":
+            global isPrivileged
+            isPrivileged = Login()
+        elif command.upper() == "LOGOUT":
+            Logout()
+        elif command.upper() == "CREATEACCT":
+            CreateAccount()
+        elif command.upper() == "DELETEACCT":
+            DeleteAccount()
+        elif command.upper() == "WITHDRAW":
+            Withdraw()
+        elif command.upper() == "DEPOSIT":
+            Deposit()
+        elif command.upper() == "TRANSFER":
+            Transfer()
+
+def Login():
+    entered = input("What account are you logging in too")
+    return entered
+
+def ReadAccounts():
+    accountsFile = open(str(sys.argv[0]))
+    print(file.read())
 
 
 
-
-
-
-
-if __name__ == "__main__":
+if __name__ == "__Main__":
     main()
